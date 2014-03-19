4f199c11edc22bcc81a9eb6bd4304051003afa48
Anthony Gabrielson <agabrielson@draper.com>
2014-03-19 11:02:16 -0400

    Prototype implementation for X86::PUSH32r

--- lib/Target/X86/X86InvISelDAG.cpp
+++ lib/Target/X86/X86InvISelDAG.cpp
@@ -66,18 +66,49 @@ SDNode* X86InvISelDAG::Transmogrify(SDNode *N) {
           break;
     }
     case X86::CMP32mi8:{
+    	// NOTE: Pattern in ARM DAG Selector is busted as not handling CPSR
+    	//       not sure how to fix, so this might just be a hack!
+    	// Pattern: (CMPri:int32 GPR:$Rn, (imm:i32):$i, pred:$p, pred:%noreg)
+    	// Emits: (ARMcmp GPR:$Rn, (imm:i32):$i)
+    	unsigned Opc = X86ISD::CMP;
+    	SDValue N0 = N->getOperand(1);
+    	SDValue N1 = N->getOperand(2);
+    	SDValue ResNode = CurDAG->getNode(Opc, SDLoc(N), MVT::Other, N0, N1);
+    	CurDAG->ReplaceAllUsesOfValueWith(SDValue(N, 0), ResNode);
+    	return NULL;
+    	break;
+    }
+    case X86::PUSH32r:{
+    	//Subtract 4 from the stack
+    	//Move the new value into place
 
-			// NOTE: Pattern in ARM DAG Selector is busted as not handling CPSR
-			//       not sure how to fix, so this might just be a hack!
-			// Pattern: (CMPri:int32 GPR:$Rn, (imm:i32):$i, pred:$p, pred:%noreg)
-			// Emits: (ARMcmp GPR:$Rn, (imm:i32):$i)
-			unsigned Opc = X86ISD::CMP;
-			SDValue N0 = N->getOperand(1);
-			SDValue N1 = N->getOperand(2);
-			SDValue ResNode = CurDAG->getNode(Opc, SDLoc(N), MVT::Other, N0, N1);
-			CurDAG->ReplaceAllUsesOfValueWith(SDValue(N, 0), ResNode);
-			return NULL;
-			break;
+    	//Get the arguments
+    	SDValue Chain = N->getOperand(0);
+    	SDValue Val = N->getOperand(1);	//Arg passes to push
+    	SDValue ESP = N->getOperand(2);
+
+    	SDLoc SL(N);
+    	SDVTList AddVTList = CurDAG->getVTList(MVT::i32);
+
+    	//Subtract
+    	SDValue Width = CurDAG->getTargetConstant(4, MVT::i32);//get constant
+    	SDValue Addr = CurDAG->getNode(ISD::SUB, SL, AddVTList, ESP, Width);	//ESP -= 4;
+    	CurDAG->ReplaceAllUsesOfValueWith(SDValue(N, 0), Addr);
+
+    	const MachineSDNode *MN = dyn_cast<MachineSDNode>(N);
+    	MachineMemOperand *MMO = NULL;
+    	if (MN->memoperands_empty()) {
+    		errs() << "NO MACHINE OPS for STR_PRE_IMM!\n";
+    	} else {
+    		MMO = *(MN->memoperands_begin());
+    	}
+
+    	//Store
+    	SDValue Store = CurDAG->getStore(Chain, SL, Val, Addr, MMO);
+    	CurDAG->ReplaceAllUsesOfValueWith(SDValue(N, 1), Store);
+
+    	return NULL;
+    	break;
     }
     /*
     case X86::MOV64rr:{
