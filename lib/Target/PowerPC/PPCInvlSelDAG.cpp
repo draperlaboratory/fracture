909252d02fb517bc1baac5a6bbd3416e29647653
EffJKay <fjk1625@draper.com>
2014-06-20 10:23:17 -0400

    implementation of ppc::STD

--- lib/Target/PowerPC/PPCInvlSelDAG.cpp
+++ lib/Target/PowerPC/PPCInvlSelDAG.cpp
@@ -52,6 +52,84 @@ SDNode* PPCInvISelDAG::Transmogrify(SDNode *N) {
     default:
       outs() << "TargetOpc: " << TargetOpc << "\n";
       break;
+    case PPC::STD:{
+
+    	/*
+    	 *
+    	 * std RS,DS(RA)
+    	 * if RA = 0 then b <- 0
+		 	 * else b <- (RA)
+		 	 * EA <- b + EXTS(DS || 0b00)
+		 	 * MEM(EA, 8) <- (RS)
+
+		 	 * Let the effective address (EA) be the sum
+		 	 * (RA|0)+ (DS||0b00). (RS) is stored into the doubleword
+		 	 * in storage addressed by EA.
+		 	*/
+
+    	SDValue Chain = N->getOperand(0);
+    	SDValue X9 = N->getOperand(1);	// register x9 a.k.a. RS
+    	SDValue DS = N->getOperand(2);	// const 48
+    	SDValue RA = N->getOperand(3);	// r31
+
+      const MachineSDNode *MN = dyn_cast<MachineSDNode>(N);
+      MachineMemOperand *MMO = NULL;
+      if (MN->memoperands_empty()) {
+        errs() << "NO MACHINE OPS for LEAVE!\n";
+      } else {
+        MMO = *(MN->memoperands_begin());
+      }
+
+      EVT LdType = N->getValueType(0);
+      SDLoc SL(N);
+
+      /* SDValue B;  TODO: branch condition
+      if (RA == 0)
+      	B = CurDAG->getConstant(0x0, LdType);
+      else*/
+      SDValue	B = CurDAG->getLoad(LdType, SL, Chain, RA, MMO); //Load from RA
+
+    	SDValue C1 = CurDAG->getConstant(0xb00, LdType);
+    	SDValue EA_or = CurDAG->getNode(ISD::OR, SL, LdType, SDValue(B.getNode(),1), DS, C1);   // DS || 0xb00
+    	// TODO: sign extend EA_or
+
+    	SDValue EA = CurDAG->getNode(ISD::ADD, SL, LdType, SDValue(EA_or.getNode(),1), EA_or, B);
+
+    	SDValue X9_val = CurDAG->getLoad(LdType, SL, SDValue(EA.getNode(),1), X9, MMO); //Load from X9
+      SDValue Store = CurDAG->getStore(SDValue(X9_val.getNode(),1), SL, X9_val, EA, MMO);
+
+    	CurDAG->ReplaceAllUsesOfValueWith(SDValue(N, 0), SDValue(Store.getNode(),1));   //Chain
+
+      //For getLoad or getStore
+      FixChainOp(B.getNode());
+      FixChainOp(X9_val.getNode());
+      FixChainOp(Store.getNode());
+
+    	return NULL;
+    	break;
+    }
+    	/*
+    case PPC::RLDICL:
+
+
+    	 * 	n <- sh5 || sh0:4
+				  r <- ROTL64((RS), n)
+					b <- mb5 || mb0:4
+					m <- MASK(b, 63)
+					RA <- r & m
+    	 */
+
+
+    	/*
+      SDValue RS = N->getOperand(0);	// register x9
+      SDValue SH = N->getOperand(1);	// const: 0
+      SDValue MB = N->getOperand(2);	// const: 32
+
+
+
+    	return NULL;
+    	break;
+    	*/
   }
 
 
