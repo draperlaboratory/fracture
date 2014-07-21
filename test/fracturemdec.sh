1502bd57d7c3f57ababdb6216063d01756145906
Dillon Forzese <forzesed@gmail.com>
2014-07-21 12:14:10 -0400

    Added -i functionality to fracturemdec to test individual instructions

--- test/fracturemdec.sh
+++ test/fracturemdec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#program that will grab all ARM binaries in a dir and check if they run, if not it will print the opc or error to results.txt
+#program that will grab all ARM binaries in a directory and check if they run, if not it will print the opc or error to results.txt
 #commented out read commands can be read like comments
 #implement stack dump
 
@@ -10,13 +10,15 @@ target=""
 stackdump=no
 targetdir=*
 checksum=no
+indivInst=no
 
-while getopts paxsdhcm:f: option
+while getopts ipaxsdhcm:f: option
 do
 	case "${option}" 
 	in
+	i)	indivInst=yes;;
 	p)	target="PPC";;
-	a)	target="ARM";;
+	a)	target="ARM" n="v6";;
 	x)	target="x86";;
 	s)	stackdump=yes;;
 	d)	stackdump=yes checksum=yes;;
@@ -30,6 +32,7 @@ do
 -m	=Set M Atribute
 -f	=Specify targed directory (If not specified the current directory will be used)
 -c	=Enable checksum print on failure
+-i	=Target individual instruction filew, will dec from 0x0 by default
 -h	=HELP" 
 		exit 0
 		;;
@@ -60,23 +63,29 @@ do
 		#echo "Filename is >>> $fn"
 		echo "sym .text		
 quit" | fracture -mattr=$n $fn>location.tmp
-		#read -p "SYM written to temp... " -s
-	if grep -q "main" location.tmp
+		#read -p "SYM written to a temp... " -s
+	if [[ $indivInst = yes ]]
+	then
+		read -p "Hey you pressed -i" -n1 -s
+		echo "dec 0x0
+quit" | fracture -mattr=$n $fn&>ircode.tmp
+		printf "$target BIN | DEC 0x0  | ">>results.txt	
+	elif grep -q "main" location.tmp
 	then	
 		#read -p "Got main, running dec..." -s		
 		echo "dec main
 quit" | fracture -mattr=$n $fn&>ircode.tmp
-		printf "ARM  ELF |  DEC MAIN  | ">>results.txt
-		#read -p "Saved dec main to a temp2..."
+		printf "$target  ELF |  DEC MAIN  | ">>results.txt
+		#read -p "Saved dec main to a temp..."
 	else	
 		#echo "binary is stripped"
 		#grab the last word 0x0000aaaa	
 		location=$(tail -c11 location.tmp)
 		#read -p "Got the location >> $location " -n1 -s
-		printf "STRIPPED | $location | ">>results.txt
+		printf "$target STRIPPED | $location | ">>results.txt
 		echo "dec $location
 quit" | fracture -mattr=$n $fn&>ircode.tmp
-		#read -p "Saved DEC to temp2... " -n1 -s				
+		#read -p "Saved DEC to a temp... " -n1 -s				
 	fi
 	
 	if grep -q "LLVM ERROR:" ircode.tmp
@@ -128,8 +137,11 @@ totalsuc=$(grep -oc "SUCCESS" results.txt)
 printf "Total successful decompiles:  $totalsuc">>results.txt
 printf "\nTotal LLVM crashes: ">>results.txt
 grep -oc "FAIL OPC" results.txt >> results.txt
+if [[ $binaries > 1 ]]
+then
 printf "Percent Success: ">>results.txt
 echo "$totalsuc $binaries" | awk '{printf "%.2f \n", $1/$2}'>>results.txt
+fi
 printf "\nTotal binaries $binaries"
 printf "\nTotal segmentation faults or aborts: "
 grep -oc "SEG FAULT" results.txt
@@ -137,9 +149,11 @@ totalsuc=$(grep -oc "SUCCESS" results.txt)
 printf "Total successful decompiles:  $totalsuc"
 printf "\nTotal LLVM crashes: "
 grep -oc "FAIL OPC" results.txt
+if [[ $binaries > 1 ]]
+then
 printf "Percent Success: "
 echo "$totalsuc $binaries" | awk '{printf "%.2f \n", $1/$2}'
-
+fi
 #clean up and delete temp files
 function finish {
 find * -type f \( -name 'location.tmp' -o -name 'ircode.tmp' -o -name 'fracturemdec.sh~' \) -delete
