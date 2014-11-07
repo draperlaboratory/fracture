b3475ab2468c2bd7a0519183930b9d397e2fb4fd
rtc-draper <rcarback@draper.com>
2014-11-07 16:41:56 -0500

    Added logic to roll up add/sub operations which feed I2P instructions. This gives aggregate offsets to add to the memory list.

--- lib/Transforms/TypeRecovery/TypeRecovery.cpp
+++ lib/Transforms/TypeRecovery/TypeRecovery.cpp
@@ -20,6 +20,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Pass.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
@@ -63,6 +64,8 @@ bool TypeRecovery::runOnFunction(Function &F) {
   // Map Src Registers to Memory lists
   std::map<Value*, MemList*> MemLists;
   std::vector<Value*> SrcRegs;
+  std::vector<Value*> NonIntOffsets;
+  std::vector<int64_t> IntOffsets;
 
   for (Function::iterator BB = F.begin(), BBE = F.end(); BB != BBE; ++BB) {
     for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
@@ -71,9 +74,42 @@ bool TypeRecovery::runOnFunction(Function &F) {
       if (isa<IntToPtrInst>(I)) {
         Value *SrcReg, *OffsetVal;
         Value *Cur = I;
+        OffsetVal = NULL;
+        int64_t IntOffset = 0;
+        // Loop till we see the global, but while we are there calculate offsets
         while(Cur != NULL && !isa<GlobalVariable>(Cur)
           && isa<Instruction>(Cur)) {
-          Cur = dyn_cast<Instruction>(Cur)->getOperand(0);
+          Instruction* CurInst = dyn_cast<Instruction>(Cur);
+          Cur = CurInst->getOperand(0);
+          if (OffsetVal != NULL) {
+            continue;
+          }
+          if (CurInst->getNumOperands() < 2) {
+            continue;
+          }
+          // Note: assuming 2nd op is always the constant and 1st is always
+          // leading to the register.
+          if (!isa<ConstantInt>(CurInst->getOperand(1))) {
+            outs() << "Non-Constant Value!!: " << *CurInst << "\n";
+            OffsetVal = CurInst->getOperand(1);
+            continue;
+          }
+          int64_t Tmp = dyn_cast<ConstantInt>(
+            CurInst->getOperand(1))->getSExtValue();
+          switch (CurInst->getOpcode()) {
+            case Instruction::Add:
+              outs() << "Add!\n";
+              break;
+            case Instruction::Sub:
+              outs() << "Sub!\n";
+              Tmp *= -1;
+              break;
+            default:
+              outs() << "Unknown opcode type! " << *CurInst << "\n";
+              break;
+          }
+          IntOffset += Tmp;
+          outs() << "IntOffset=" << IntOffset << "\n";
         }
         if (Cur != NULL && isa<GlobalVariable>(Cur)) {
           SrcReg = Cur;
@@ -87,6 +123,15 @@ bool TypeRecovery::runOnFunction(Function &F) {
   for (int i = 0, e = SrcRegs.size(); i != e; ++i) {
     outs() << *SrcRegs[i] << "\n";
   }
+  outs() << "Non-Int Offsets: \n";
+  for (int i = 0, e = NonIntOffsets.size(); i != e; ++i) {
+    outs() << *NonIntOffsets[i] << "\n";
+  }
+  outs() << "Int Offsets: \n";
+  for (int i = 0, e = IntOffsets.size(); i != e; ++i) {
+    outs() << IntOffsets[i] << "\n";
+  }
+
 
   return false;
 }
