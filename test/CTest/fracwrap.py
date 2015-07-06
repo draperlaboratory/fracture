b922a48e0862babff0e75d04d4990ed9e0b62fbe
Ming <ming@draper.com>
2015-07-06 18:40:44 -0400

    first add of a simplified fracture wrapper to dis/dec all symbols in a binary

---
+++ test/CTest/fracwrap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+
+# simple wrapper for thorough execution of fracture on a binary
+# with pass/fail notification on a clear execution failure
+
+def main:
+  #...
+
+  exec_frac()
+
+
+
+
+def exec_frac(args):
+  #asdf
+
+  return (failed, signal)
+
+main()
